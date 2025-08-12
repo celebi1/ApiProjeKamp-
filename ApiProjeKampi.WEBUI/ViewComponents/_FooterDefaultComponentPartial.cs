086eb3b8a1043f822c8be3dd3c62128732739772
Çelebi Çeri <clbceri@gmail.com>
2025-08-12 17:42:17 +0300

    s

---
+++ ApiProjeKampi.WEBUI/ViewComponents/_FooterDefaultComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WEBUI.ViewComponents
+{
+    public class _FooterDefaultComponentPartial :ViewComponent
+    {
+        public IViewComponentResult Invoke() {  return View(); }
+    }
+}
